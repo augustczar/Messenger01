1b9d2612b3b80023a1a8b4162495b726b8378036
augustczar <augustczarramos@gmail.com>
2023-11-26 20:10:15 -0300

    Initial Project

---
+++ Messenger01/src/main/java/com/augustczar/messenger/Messenger01Application.java
@@ -0,0 +1,13 @@
+package com.augustczar.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Messenger01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Messenger01Application.class, args);
+	}
+
+}
---
+++ Messenger01/src/test/java/com/augustczar/messenger/Messenger01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustczar.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Messenger01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
