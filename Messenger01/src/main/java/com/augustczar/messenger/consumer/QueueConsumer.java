0df0a7cb06f2ae418e3e558205c507d8b5626bea
augustczar <augustczarramos@gmail.com>
2023-11-27 15:46:37 -0300

    update

--- Messenger01/src/main/java/com/augustczar/messenger/consumer/QueueConsumer.java
+++ Messenger01/src/main/java/com/augustczar/messenger/consumer/QueueConsumer.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class QueueConsumer {
-	@RabbitListener(queues= {"${queue.name}"})
+	@RabbitListener(queues= "${queue.name}")
 	public void receive(@Payload String fileBody) {
 		System.out.println("Message: " +fileBody);
 	}
